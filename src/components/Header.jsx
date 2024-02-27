7ef2451cf09a8cbea969a2da8f211c506252bef9
bmaxtar <bmaxtar@gmail.com>
2024-02-27 17:24:34 +0000

    create login, register, forgot-password pages & components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
   const location = useLocation()
   const navigate = useNavigate()
 
-  function pathMathRoute(route) {
+  function pathMatchRoute(route) {
     if (route === location.pathname) {
       return true
     }
@@ -15,21 +15,21 @@ export default function Header() {
     <div className='header bg-white border-b shadow-sm sticky top-0 z-50'>
       <header className='header-menu flex justify-between items-center px-3 max-w-6xl mx-auto'>
         <div className="header-menu logo-img">
-          <img src={require('../images/logo.png')} alt="realtor-clone-app-with-react-app-logo" className='h-5 cursor-pointer' onClick={() => navigate("/")} />
+          <img src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg" alt="realtor-clone-app-with-react-app-logo" className='h-5 cursor-pointer' onClick={() => navigate("/")} />
         </div>
         <div className="menu-header">
           <ul className='header-menu-ul flex space-x-10'>
-            <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/") && "text-black border-b-red-500"}`}
+            <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/") && "text-black border-b-red-500"}`}
               onClick={() => navigate("/")}
             >
               Home
             </li>
-            <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/offers") && "text-black border-b-red-500"}`}
+            <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/offers") && "text-black border-b-red-500"}`}
               onClick={() => navigate("/offers")}
             >
               Offers
             </li>
-            <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/register") && "text-black border-b-red-500"}`}
+            <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/register") && "text-black border-b-red-500"}`}
               onClick={() => navigate("/register")}
             >
               Sign in
--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,7 +1,90 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+import { Link } from 'react-router-dom';
+import OAuth from '../../components/OAuth';
 
 export default function Register() {
+  const [showPassword, setShowPassword] = useState(false)
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  })
+
+  const { name, email, password } = formData
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value
+    }))
+  }
+
   return (
-    <div>Signup</div>
+    <section>
+      <h1 className="text-3xl text-center font-bold mt-6">Sign Up </h1>
+      <div className='flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto'>
+        <div className="md:w-[67%] lg:w-[50%] mb-12 mb:mb-6 ">
+          <img src={require("../../images/key.jpeg")} alt="realtor-clone-app-login-img"
+            className="w-full rounded-2xl"
+          />
+        </div>
+        <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
+          <form>
+            <input className='w-full px-4 py-2 mb-6 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out'
+              type="text" id='name'
+              placeholder='Full name'
+              value={name} onChange={onChange}
+            />
+            <input className='w-full px-4 py-2 mb-6 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out'
+              type="email" id='email'
+              placeholder='Email address'
+              value={email} onChange={onChange}
+            />
+            <div className="input-group-password relative mb-6">
+              <input className='w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out'
+                type={showPassword ? "text" : "password"} id='password'
+                placeholder='Password'
+                value={password} onChange={onChange}
+              />
+              {showPassword ? (
+                <AiFillEyeInvisible className="input-icon absolute right-3 top-3 text-xl cursor-pointer"
+                  onClick={() => setShowPassword((prevState) => !prevState)}
+                />
+              ) : <AiFillEye className="input-icon absolute right-3 top-3 text-xl cursor-pointer"
+                onClick={() => setShowPassword((prevState) => !prevState)}
+              />
+              }
+            </div>
+            <div className='flex justify-between whitespace-nowrap text-sm sm:text-lg'>
+              <p className='mb-6' >Have an account ?
+                <Link to="/login" className='text-red-600 hover:text-red-700
+                 duration-200 ease-in-out ml-1'>
+                  Sign In
+                </Link>
+              </p>
+              <p>
+                <Link to="/forgot-password" className='text-blue-600
+                 hover:text-blue-800 duration-200 ease-in-out ml-1'>
+                  Forgot password ?
+                </Link>
+              </p>
+            </div>
+            <button type="submit" className='w-full bg-blue-600 text-white px-7 py-3 text-sm 
+          font-medium uppercase rounded shadow-md hover:bg-blue-700 transition ease-in-out duration-300
+          hover:shadow-lg active:to-blue-800 active:duration-200'
+            >
+              Sign up
+            </button>
+            <div className='flex  items-center my-4 before:border-t  before:flex-1 before:border-gray-300
+           after:border-t after:flex-1 after:border-gray-300'>
+              <p className='text-center font-semibold mx-4'>OR</p>
+            </div>
+            <OAuth />
+          </form>
+        </div>
+      </div >
+    </section >
   )
 }
